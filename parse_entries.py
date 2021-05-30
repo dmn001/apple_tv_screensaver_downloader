e15e2b940083ea1b4f6e0e80ed2c5b4c80e455b1
Dave Man <dmn001@gmail.com>
2021-05-30 15:34:47 +0100

    cleanup

--- parse_entries.py
+++ parse_entries.py
@@ -5,8 +5,6 @@ import operator
 with open('entries.json') as json_data:
     json_obj = json.load(json_data)
 
-# print json_obj
-
 filename_for_url = {}
 
 assets = json_obj['assets']
@@ -20,30 +18,22 @@ for item in assets:
         base_filename = m.group(1)
 
     filename = filename + " " + base_filename
-    # print filename
-
     filename_for_url[url] = filename
 
 file = open('urls.csv', "wb")
-# for url in sorted(filename_for_url):
 for x in sorted(filename_for_url.items(), key=operator.itemgetter(1)):
-    # out_str = "%s,%s" % (url,filename_for_url[url])
     out_str = "%s,%s" % (x[0],x[1])
-    # print out_str
     file.write(out_str + "\n")
 file.close()
 
 file = open('urls_aria.txt', "wb")
-# for url in sorted(filename_for_url):
 for x in sorted(filename_for_url.items(), key=operator.itemgetter(1)):
-    # out_str = "%s\n out=%s" % (url,filename_for_url[url])
     out_str = "%s\n out=%s" % (x[0],x[1])
     file.write(out_str + "\n")
 file.close()
 
 file = open('filenames.txt',"wb")
 for x in sorted(filename_for_url.items(), key=operator.itemgetter(1)):
-    # out_str = "%s\n out=%s" % (url,filename_for_url[url])
     out_str = "%s" % (x[1])
     file.write(out_str + "\n")
 file.close()
